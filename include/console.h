5ed3e62a6c63e72ed160481a8136662ba8511ad4
Vodoku <33237549+Vodoku@users.noreply.github.com>
2017-12-27 12:06:49 -0500

    Setting up console class

---
+++ include/console.h
@@ -0,0 +1,9 @@
+#ifndef CONSOLE_H 
+#define CONSOLE_H 
+
+class Console 
+{
+
+};
+
+#endif // CONSOLE_H 
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream> 
 #include <string> 
-
 #include "config.h"
-
 #ifdef QT5_PACKAGES_FOUND
 #include <QApplication> 
 #include "window.h" 
+#else 
+#include "console.h"
 #endif // QT5_PACKAGES_FOUND 
 
 int main(int argc, char *argv[]) 
 {
 #ifdef QT5_PACKAGES_FOUND 
-
+    // If user has defined QT Packages to be included 
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication application; 
+    QWidget widget; 
+    widget.show(); 
+    return application.exec(); 
 #else // QT_PACKAGES_FOUND 
-    std::cout << "Hello program!" << std::endl; 
+    
     return 0; 
 #endif // QT_PACKAGES_FOUND 
 }
