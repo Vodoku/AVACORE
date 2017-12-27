94d2a4109a49a5eb0c8c38b1a9bbd2cfa3a838d3
Vodoku <33237549+Vodoku@users.noreply.github.com>
2017-12-27 15:06:14 -0500

    setup console.h

--- include/console.h
+++ include/console.h
@@ -1,8 +1,40 @@
 #ifndef CONSOLE_H 
 #define CONSOLE_H 
 
+#include <iostream> 
+using std::string;
+using std::cout; 
+using std::cin; 
+#include <sstream> 
+#include <fstream> 
+#include <string> 
+#include <vector> 
+#include <map> 
+
 class Console 
 {
+    public: 
+        static int const FLAG_WARNING = OutputStatus.WARNING; 
+        static int const 
+        Console(void); 
+       ~Console(void); 
+
+        bool running(void) const;
+
+        void pause(void); 
+        void exit(void);
+
+    private: 
+        // Types of output 
+        enum OutputStatus
+        {
+            WARNING = 0, 
+            NORMAL  = 1, 
+            ERROR   = 2
+        }
+
+    private: 
+        bool m_running; 
 
 };
 
