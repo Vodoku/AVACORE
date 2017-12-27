5ed9f4f6e602113e0ba1a59a50a1ae6af6d94f38
Vodoku <33237549+Vodoku@users.noreply.github.com>
2017-12-26 19:48:31 -0500

    Update: Initial Dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream> 
 #include <string> 
-#include <config.h> 
+
+#include "config.h"
+
+#ifdef QT5_PACKAGES_FOUND
+#include <QApplication> 
+#include "window.h" 
+#endif // QT5_PACKAGES_FOUND 
 
 int main(int argc, char *argv[]) 
 {
+#ifdef QT5_PACKAGES_FOUND 
+
+#else // QT_PACKAGES_FOUND 
+    std::cout << "Hello program!" << std::endl; 
     return 0; 
+#endif // QT_PACKAGES_FOUND 
 }
